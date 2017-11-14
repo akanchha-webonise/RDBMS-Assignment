eda8c4b6d29aaebd2370e9e555e504ee02b63534
akanchhabhadauria-webonise <akanchha.bhadauria@weboniselab.com>
2017-11-14 21:00:27 +0530

    PL SQL ASSIGNMENT

--- ecommerce_application.sql
+++ ecommerce_application.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.5.58, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: ecommerce_app
+-- Host: localhost    Database: ecommerce_application
 -- ------------------------------------------------------
 -- Server version	5.5.58-0ubuntu0.14.04.1
 
@@ -16,27 +16,96 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `colors`
+-- Table structure for table `carts`
 --
 
-DROP TABLE IF EXISTS `colors`;
+DROP TABLE IF EXISTS `carts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `colors` (
-  `color_name` varchar(150) NOT NULL,
-  `color_id` int(11) NOT NULL,
-  PRIMARY KEY (`color_id`)
+CREATE TABLE `carts` (
+  `user_id` int(11) DEFAULT NULL,
+  `colour_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `product_quantity` int(11) DEFAULT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `colors`
+-- Dumping data for table `carts`
 --
 
-LOCK TABLES `colors` WRITE;
-/*!40000 ALTER TABLE `colors` DISABLE KEYS */;
-/*!40000 ALTER TABLE `colors` ENABLE KEYS */;
+LOCK TABLES `carts` WRITE;
+/*!40000 ALTER TABLE `carts` DISABLE KEYS */;
+INSERT INTO `carts` VALUES (2,4,2,5,2,'2017-11-14 00:00:00','2017-11-13 18:30:00'),(3,2,1,10,3,'2017-11-14 00:00:00','2017-11-13 18:30:00'),(1,5,4,10,4,'2017-11-14 00:00:00','2017-11-13 18:30:00'),(5,3,2,5,5,'2017-11-14 00:00:00','2017-11-13 18:30:00'),(1,1,1,5,1,'2017-11-14 00:00:00','2017-11-13 18:30:00');
+/*!40000 ALTER TABLE `carts` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Temporary table structure for view `monthly_report`
+--
+
+DROP TABLE IF EXISTS `monthly_report`;
+/*!50001 DROP VIEW IF EXISTS `monthly_report`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `monthly_report` (
+  `id` tinyint NOT NULL,
+  `order_date` tinyint NOT NULL,
+  `product_name` tinyint NOT NULL,
+  `price` tinyint NOT NULL,
+  `checkout_cost` tinyint NOT NULL,
+  `user_name` tinyint NOT NULL,
+  `email` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `order_history`
+--
+
+DROP TABLE IF EXISTS `order_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_history` (
+  `user_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `product_quantity` int(11) NOT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `colour_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_history`
+--
+
+LOCK TABLES `order_history` WRITE;
+/*!40000 ALTER TABLE `order_history` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_history` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary table structure for view `order_view`
+--
+
+DROP TABLE IF EXISTS `order_view`;
+/*!50001 DROP VIEW IF EXISTS `order_view`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `order_view` (
+  `id` tinyint NOT NULL,
+  `final_cost` tinyint NOT NULL,
+  `order_date` tinyint NOT NULL,
+  `discount_coupon` tinyint NOT NULL,
+  `payment_type` tinyint NOT NULL,
+  `payment_status` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `orders`
@@ -46,15 +115,15 @@ DROP TABLE IF EXISTS `orders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `orders` (
-  `shipping_id` int(11) DEFAULT NULL,
-  `quantity` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `order_cost` int(11) NOT NULL,
-  `date` date NOT NULL,
-  `product_id` int(11) DEFAULT NULL,
-  `discount` int(11) NOT NULL,
-  `transaction_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`order_id`)
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `order_date` date DEFAULT NULL,
+  `order_status` varchar(20) DEFAULT NULL,
+  `final_cost` decimal(10,2) DEFAULT NULL,
+  `shipping_date` date DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -64,6 +133,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,1,'2017-11-14','Placed',5750.00,'2017-11-17','2017-11-14 00:00:00','2017-11-14 14:59:55'),(2,2,'2017-11-14','Dispatched',1750.00,'2017-11-18','2017-11-14 00:00:00','2017-11-13 18:30:00'),(3,3,'2017-11-13','Placed',2800.00,'2017-11-15','2017-11-14 00:00:00','2017-11-14 15:01:33'),(4,1,'2017-11-13','Delivered',NULL,'2017-11-18','2017-11-14 00:00:00','2017-11-14 15:01:33'),(5,5,'2017-11-13','Placed',1200.00,'2017-11-16','2017-11-14 00:00:00','2017-11-13 18:30:00');
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -75,11 +145,16 @@ DROP TABLE IF EXISTS `payments`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `payments` (
-  `payment_status` varchar(255) NOT NULL,
-  `date` date NOT NULL,
-  `transaction_id` int(11) NOT NULL,
-  `payment_mode` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`transaction_id`)
+  `id` int(11) NOT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `payment_type` varchar(50) NOT NULL,
+  `discount_coupon` decimal(3,2) DEFAULT NULL,
+  `payment_date` date DEFAULT NULL,
+  `payment_status` varchar(50) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `checkout_cost` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -89,6 +164,7 @@ CREATE TABLE `payments` (
 
 LOCK TABLES `payments` WRITE;
 /*!40000 ALTER TABLE `payments` DISABLE KEYS */;
+INSERT INTO `payments` VALUES (1,1,'COD',9.99,'2017-11-14','PENDING','2017-11-14 00:00:00','2017-11-14 15:02:27',5175.00),(2,2,'NETBANKING',9.99,'2017-11-14','RECEIVED','2017-11-14 00:00:00','2017-11-14 15:02:27',1575.00),(3,3,'NETBANKING',9.99,'2017-11-14','RECEIVED','2017-11-14 00:00:00','2017-11-14 15:02:27',2520.00),(4,4,'COD',9.99,'2017-11-14','PENDING','2017-11-14 00:00:00','2017-11-14 15:01:33',NULL),(5,5,'COD',9.99,'2017-11-14','PENDING','2017-11-14 00:00:00','2017-11-14 15:02:27',1080.00);
 /*!40000 ALTER TABLE `payments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -100,12 +176,11 @@ DROP TABLE IF EXISTS `products`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `products` (
-  `product_name` varchar(255) NOT NULL,
-  `price` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `color_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`product_id`)
+  `id` int(11) NOT NULL,
+  `product_name` varchar(20) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -115,32 +190,8 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'shirt','2017-11-13 00:00:00','2017-11-12 18:30:00'),(2,'skirt','2017-11-13 00:00:00','2017-11-12 18:30:00'),(3,'top','2017-11-13 00:00:00','2017-11-12 18:30:00'),(4,'shoes','2017-11-13 00:00:00','2017-11-12 18:30:00'),(5,'sweater','2017-11-13 00:00:00','2017-11-12 18:30:00');
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `shipping_info`
---
-
-DROP TABLE IF EXISTS `shipping_info`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `shipping_info` (
-  `shipping_address` varchar(255) NOT NULL,
-  `shipping_id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `postal_code` int(11) NOT NULL,
-  PRIMARY KEY (`shipping_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `shipping_info`
---
-
-LOCK TABLES `shipping_info` WRITE;
-/*!40000 ALTER TABLE `shipping_info` DISABLE KEYS */;
-/*!40000 ALTER TABLE `shipping_info` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -151,12 +202,16 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `user_name` varchar(255) NOT NULL,
-  PRIMARY KEY (`user_id`),
+  `id` int(11) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `email` varchar(15) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `user_name` varchar(25) NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -167,8 +222,76 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'buyer','akansha@gmail.c','root','akansha','9866431315','123 Bavdhan','2017-11-10 18:11:56','2017-11-13 14:03:32'),(2,'buyer','kesari@gmail.co','root1','kesari','9866412315','Bavdhan','2017-11-10 18:15:56','2017-11-13 14:03:48'),(3,'inverntory manager','shiwani@gmail.c','root2','shiwani','9866412308','Baner','2017-11-10 18:30:56','2017-11-13 14:03:59'),(4,'buyer','shamli@gmail.co','root3','shamli','9866214308','Pashan','2017-11-10 18:32:56','2017-11-13 14:04:14'),(5,'inventory manager','aditi@gmail.com','root4','aditi','9866213608','Dighi','2017-11-10 18:35:56','2017-11-13 14:04:21');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `variants`
+--
+
+DROP TABLE IF EXISTS `variants`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `variants` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `colour_name` varchar(10) NOT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `variants`
+--
+
+LOCK TABLES `variants` WRITE;
+/*!40000 ALTER TABLE `variants` DISABLE KEYS */;
+INSERT INTO `variants` VALUES (1,1,'red',10,250.00,'2017-11-13 00:00:00','2017-11-12 18:30:00'),(2,1,'black',20,280.00,'2017-11-13 00:00:00','2017-11-12 18:30:00'),(3,2,'black',20,240.00,'2017-11-13 00:00:00','2017-11-12 18:30:00'),(4,3,'black',15,350.00,'2017-11-13 00:00:00','2017-11-12 18:30:00'),(5,4,'white',10,450.00,'2017-11-13 00:00:00','2017-11-12 18:30:00'),(6,5,'blue',50,850.00,'2017-11-13 00:00:00','2017-11-12 18:30:00');
+/*!40000 ALTER TABLE `variants` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Final view structure for view `monthly_report`
+--
+
+/*!50001 DROP TABLE IF EXISTS `monthly_report`*/;
+/*!50001 DROP VIEW IF EXISTS `monthly_report`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `monthly_report` AS select `o`.`id` AS `id`,`o`.`order_date` AS `order_date`,`p`.`product_name` AS `product_name`,`v`.`price` AS `price`,`pymt`.`checkout_cost` AS `checkout_cost`,`u`.`user_name` AS `user_name`,`u`.`email` AS `email` from (((((`orders` `o` join `products` `p`) join `variants` `v`) join `payments` `pymt`) join `users` `u`) join `order_history` `oh`) where ((`oh`.`user_id` = `u`.`id`) and (`oh`.`colour_id` = `v`.`id`) and (`oh`.`product_id` = `p`.`id`) and (`oh`.`order_id` = `o`.`id`) and (`oh`.`order_id` = `pymt`.`order_id`)) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `order_view`
+--
+
+/*!50001 DROP TABLE IF EXISTS `order_view`*/;
+/*!50001 DROP VIEW IF EXISTS `order_view`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `order_view` AS select `o`.`id` AS `id`,`o`.`final_cost` AS `final_cost`,`o`.`order_date` AS `order_date`,`p`.`discount_coupon` AS `discount_coupon`,`p`.`payment_type` AS `payment_type`,`p`.`payment_status` AS `payment_status` from (`orders` `o` join `payments` `p`) where (`o`.`id` = `p`.`order_id`) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -179,4 +302,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-13 14:51:45
+-- Dump completed on 2017-11-14 20:48:27
